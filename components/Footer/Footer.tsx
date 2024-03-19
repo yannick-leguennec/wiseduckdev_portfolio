c1be0a15fd0030090c0e9e52a39e0b27602ec43c
Wise Dev <wiseduckdev@gmail.com>
2024-03-19 08:22:15 -0400

    :tada: Added a link to the portfolio page in the Footer, and made it dynamic in order to see optimize the user experience and navigation

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,17 +1,36 @@
 import classes from "./Footer.module.scss";
 import { useLanguage } from "../../context/LanguageContext";
+import { TranslationsType } from "../../types/TranslationsType";
+import { useRouter } from "next/router";
 import { FaGithub } from "react-icons/fa";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaRobot } from "react-icons/fa";
+import Image from "next/image";
+import logo from "../../public/images/logos/simple_logo_white.png";
 
 function Footer() {
   // Custom hook to manage the language changes
   const { activeLanguage } = useLanguage();
+  // Router
+  const router = useRouter();
+  const { pathname, locale } = router;
   // Site URL
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
+  // Logic to determine if we are on the homepage or GPTs page
+  const isHomePage = pathname === "/" || pathname === "/fr";
+  const isGptsPage = pathname === "/gpts" || pathname === "/fr/gpts";
   // Determine the GPTs link based on the active language
   const gptsLink = activeLanguage === "FR" ? `/fr/gpts` : `/gpts`;
+  const mainLink = activeLanguage === "FR" ? `/fr` : `/`;
+
+  // Object to store the translations for the alt attribute of the logo
+  const translationAlt: TranslationsType = {
+    altLogo: {
+      EN: "Certified Full Stack JavaScript Web and Web Mobile Developer specialized in React Wise Duck Dev Logo",
+      FR: "Logo du développeur certifié web et web mobile Full Stack JavaScript spécialisé en React Wise Duck Dev",
+    },
+  };
 
   return (
     <footer className={classes.footer}>
@@ -39,9 +58,20 @@ function Footer() {
         >
           <FaXTwitter className={classes.logo} />
         </a>
-        <a href={gptsLink} rel="noopener noreferrer">
-          <FaRobot className={classes.logo} />
-        </a>
+        {isHomePage && (
+          <a href={gptsLink} rel="noopener noreferrer">
+            <FaRobot className={classes.logo} />
+          </a>
+        )}
+        {isGptsPage && (
+          <a href={mainLink} rel="noopener noreferrer">
+            <Image
+              src={logo}
+              alt={translationAlt.altLogo[activeLanguage]}
+              className={classes.logo}
+            />
+          </a>
+        )}
       </div>
       <div className={classes.textContainer}>
         <p>© 2024 Wise Duck Dev</p>
--- pages/gpts/index.tsx
+++ pages/gpts/index.tsx
@@ -6,12 +6,17 @@ import { useLanguage } from "../../context/LanguageContext";
 import { TranslationsType } from "../../types/TranslationsType";
 import main_picture from "../../public/images/gpts/duck-mib-main.webp";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export default function GPTS() {
   return (
     <>
       <Header />
       <h1>Hello GPTS</h1>
+      <h1>Hello GPTS</h1>
+      <h1>Hello GPTS</h1>
+      <h1>Hello GPTS</h1>
+      <Footer />
     </>
   );
 }
