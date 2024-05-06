e2c8e562a880f96b82df5657f885f3ff577d70f9
The Wise Duck Dev <wiseduckdev@gmail.com>
2024-05-06 09:39:28 -0400

    :muscle: fixed a bug in the sitemap script

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -192,12 +192,13 @@ async function generateSitemap() {
   }
 
   const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-      ${urlEntries}
-    </urlset>
-  `;
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml">  // Added namespace declaration for XHTML
+    ${urlEntries}
+  </urlset>
+`;
 
   await fs.writeFile(
     path.join(process.cwd(), "public", "sitemap.xml"),
